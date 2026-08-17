35233fb98a010620e507bdc6f0b41c13a4f0ef68
Nandu0905 <103937753+Nandu0905@users.noreply.github.com>
2026-08-17 18:31:58 +0530

    Add: minimum-array-end - submission-0

---
+++ Data Structures & Algorithms/minimum-array-end/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long minEnd(int n, int x) {
+        long res =x;
+        long extra = n-1;
+        int bit=0;
+        while(extra>0){
+            if((x&(1L<<bit))==0){
+                if((extra&1)==1){
+                    res = res|(1L<<bit);
+                }
+                extra = extra>>1;
+
+            }
+            bit++;
+        }
+        return res;
+    }
+}
