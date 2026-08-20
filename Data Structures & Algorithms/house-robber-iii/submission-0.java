7460d789de1bad0818eced64a54572189f6356ce
Nandu0905 <103937753+Nandu0905@users.noreply.github.com>
2026-08-20 12:47:18 +0530

    Add: house-robber-iii - submission-0

---
+++ Data Structures & Algorithms/house-robber-iii/submission-0.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int[] res = dfs(root);
+        return Math.max(res[0],res[1]);
+    }
+    int[] dfs(TreeNode root){
+        if(root==null){
+            return new int[]{0,0};
+        }
+        int[] left = dfs(root.left);
+        int[] right = dfs(root.right);
+        int pick = root.val+left[1]+right[1];
+        int notPick = Math.max(left[0],left[1])+Math.max(right[0],right[1]);
+        return new int[]{pick,notPick};
+    }
+}
