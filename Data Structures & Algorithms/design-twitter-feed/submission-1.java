6147810289b5a18caa2266175588291376a0d6dd
Nandu0905 <103937753+Nandu0905@users.noreply.github.com>
2026-08-27 16:56:33 +0530

    Add: design-twitter-feed - submission-1

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-1.java
@@ -0,0 +1,55 @@
+class Twitter {
+    Map<Integer,List<int[]>> map = new HashMap<>();
+    Map<Integer,Set<Integer>> followerMap = new HashMap<>();
+    int counter;
+    public Twitter() {
+        
+    }
+    
+    public void postTweet(int userId, int tweetId) {
+        counter++;
+        map.computeIfAbsent(userId, k->new ArrayList<>()).add(new int[]{tweetId,counter});
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        PriorityQueue<Map.Entry<Integer,Integer>> pq=new PriorityQueue<>((a,b)->Integer.compare(b.getValue(),a.getValue()));
+        if(map.containsKey(userId)){
+            List<int[]> userOwn = map.get(userId);
+        for(int[] n:userOwn){
+                pq.offer(new AbstractMap.SimpleEntry<>(n[0],n[1]));
+            }
+        }        
+        if(followerMap.containsKey(userId)){
+            Set<Integer> userList= followerMap.get(userId);
+            for(int user:userList){
+                if(map.containsKey(user)){
+                    List<int[]> list = map.get(user);
+                    for(int[] num:list){
+                        pq.offer(new AbstractMap.SimpleEntry<>(num[0],num[1]));
+                    }
+                }
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        int count=0;
+        while(!pq.isEmpty() && count<10){
+            res.add(pq.poll().getKey());
+            count++;
+        }
+        return res;           
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        followerMap.computeIfAbsent(followerId,k->new HashSet<>()).add(followeeId);
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        if(followerMap.containsKey(followerId))
+        {
+            Set<Integer> list =followerMap.get(followerId);
+            if(list.contains(followeeId)){
+                list.remove(followeeId);
+            }
+        }
+    }
+}
